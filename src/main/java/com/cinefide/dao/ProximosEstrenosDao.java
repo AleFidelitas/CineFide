8c85cb9108ce4113937b5f10934f4f6c0c63b7c3
AleFidelitas <rjimenez90503@ufide.ac.cr>
2024-04-02 21:43:10 -0600

    test
    test

--- src/main/java/com/cinefide/dao/ProximosEstrenosDao.java
+++ src/main/java/com/cinefide/dao/ProximosEstrenosDao.java
@@ -1,8 +1,8 @@
 package com.cinefide.dao;
 
-import com.cinefide.domain.Cartelera;
+import com.cinefide.domain.proximosEstrenos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CarteleraDao extends JpaRepository<Cartelera,Long>{
+public interface proximosEstrenosDao extends JpaRepository<proximosEstrenos,Long>{
     
 }
