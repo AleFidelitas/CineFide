0d7c9ef9f62a49ccda75f2572dfd7c53946a04a4
AleFidelitas <rjimenez90503@ufide.ac.cr>
2024-04-02 21:43:23 -0600

    Update ProximosEstrenosService.java

--- src/main/java/com/cinefide/service/ProximosEstrenosService.java
+++ src/main/java/com/cinefide/service/ProximosEstrenosService.java
@@ -1,17 +1,17 @@
 package com.cinefide.service;
 
-import com.cinefide.domain.Cartelera;
+import com.cinefide.domain.proximosEstrenos;
 import java.util.List;
 
-public interface CarteleraService {
+public interface proximosEstrenosService {
     
-    public List<Cartelera> getCartelera(boolean activo);
+    public List<proximosEstrenos> getProximosEstrenos(boolean activo);
     
-    public Cartelera getCartelera(Cartelera cartelera);
+    public proximosEstrenos getProximosEstrenos(proximosEstrenos proximosEstrenos);
     
-    public void save(Cartelera cartelera);
+    public void save(proximosEstrenos proximosestrenos);
 
-    public void delete (Cartelera cartelera);
+    public void delete (proximosEstrenos proximosestrenos);
     
     
 }
