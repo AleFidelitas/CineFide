bf7e0049b5d2740fe29af2cc64ee2193b239c0cc
AleFidelitas <rjimenez90503@ufide.ac.cr>
2024-04-23 21:27:32 -0600

    Update ProximosEstrenosServiceImpl.java

--- src/main/java/com/cinefide/service/Impl/ProximosEstrenosServiceImpl.java
+++ src/main/java/com/cinefide/service/Impl/ProximosEstrenosServiceImpl.java
@@ -1,8 +1,5 @@
 package com.cinefide.service.Impl;
 
-import com.cinefide.dao.CarteleraDao;
-import com.cinefide.domain.Cartelera;
-import com.cinefide.service.CarteleraService;
 import com.cinefide.dao.proximosEstrenosDao;
 import com.cinefide.domain.proximosEstrenos;
 import com.cinefide.service.proximosEstrenosService;
@@ -12,17 +9,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class CarteleraServiceImpl implements CarteleraService{
 public class proximosEstrenosServiceImpl implements proximosEstrenosService{
     
     @Autowired
-    private CarteleraDao carteleraDao;
     private proximosEstrenosDao proximosEstrenosDao;
 
     @Override
     @Transactional(readOnly = true)
-    public List<Cartelera> getCartelera(boolean activo) {
-        var lista=carteleraDao.findAll();
     public List<proximosEstrenos> getProximosEstrenos(boolean activo) {
         var lista = proximosEstrenosDao.findAll();
 
@@ -31,24 +24,18 @@ public class proximosEstrenosServiceImpl implements proximosEstrenosService{
 
     @Override
     @Transactional(readOnly = true)
-    public Cartelera getCartelera(Cartelera cartelera) {
-        return carteleraDao.findById(cartelera.getIdPelicula()).orElse(null);
     public proximosEstrenos getProximosEstrenos(proximosEstrenos proximosestrenos) {
         return proximosEstrenosDao.findById(proximosestrenos.getIdPelicula()).orElse(null);
     }
 
     @Override
     @Transactional
-    public void save(Cartelera cartelera) {
-        carteleraDao.save(cartelera);
     public void save(proximosEstrenos proximosestrenos) {
         proximosEstrenosDao.save(proximosestrenos);
     }
 
     @Override
     @Transactional
-    public void delete(Cartelera cartelera) {
-        carteleraDao.delete(cartelera);
     public void delete(proximosEstrenos proximosestrenos) {
         proximosEstrenosDao.delete(proximosestrenos);
     }
