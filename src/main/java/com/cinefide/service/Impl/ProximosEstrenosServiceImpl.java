964823de1b03a757736b63d1caa86b8423ae0bda
AleFidelitas <rjimenez90503@ufide.ac.cr>
2024-04-23 21:27:09 -0600

    Update ProximosEstrenosServiceImpl.java

--- src/main/java/com/cinefide/service/Impl/ProximosEstrenosServiceImpl.java
+++ src/main/java/com/cinefide/service/Impl/ProximosEstrenosServiceImpl.java
@@ -3,6 +3,9 @@ package com.cinefide.service.Impl;
 import com.cinefide.dao.CarteleraDao;
 import com.cinefide.domain.Cartelera;
 import com.cinefide.service.CarteleraService;
+import com.cinefide.dao.proximosEstrenosDao;
+import com.cinefide.domain.proximosEstrenos;
+import com.cinefide.service.proximosEstrenosService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,14 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CarteleraServiceImpl implements CarteleraService{
+public class proximosEstrenosServiceImpl implements proximosEstrenosService{
     
     @Autowired
     private CarteleraDao carteleraDao;
+    private proximosEstrenosDao proximosEstrenosDao;
 
     @Override
     @Transactional(readOnly = true)
     public List<Cartelera> getCartelera(boolean activo) {
         var lista=carteleraDao.findAll();
+    public List<proximosEstrenos> getProximosEstrenos(boolean activo) {
+        var lista = proximosEstrenosDao.findAll();
 
         return lista;
     }
@@ -26,18 +33,24 @@ public class CarteleraServiceImpl implements CarteleraService{
     @Transactional(readOnly = true)
     public Cartelera getCartelera(Cartelera cartelera) {
         return carteleraDao.findById(cartelera.getIdPelicula()).orElse(null);
+    public proximosEstrenos getProximosEstrenos(proximosEstrenos proximosestrenos) {
+        return proximosEstrenosDao.findById(proximosestrenos.getIdPelicula()).orElse(null);
     }
 
     @Override
     @Transactional
     public void save(Cartelera cartelera) {
         carteleraDao.save(cartelera);
+    public void save(proximosEstrenos proximosestrenos) {
+        proximosEstrenosDao.save(proximosestrenos);
     }
 
     @Override
     @Transactional
     public void delete(Cartelera cartelera) {
         carteleraDao.delete(cartelera);
+    public void delete(proximosEstrenos proximosestrenos) {
+        proximosEstrenosDao.delete(proximosestrenos);
     }
     
 }
